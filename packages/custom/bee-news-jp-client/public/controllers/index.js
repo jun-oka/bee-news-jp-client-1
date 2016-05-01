bda369e372b34eea63775d0c68b888a33a48dc42
shiga yuichi <yuichi.shiga@gmail.com>
2016-05-01 16:21:35 -0700

    add map bbs qa

--- packages/custom/bee-news-jp-client/public/controllers/index.js
+++ packages/custom/bee-news-jp-client/public/controllers/index.js
@@ -4,13 +4,27 @@ angular.module('mean.system')
   .controller('IndexController', ['$rootScope', '$scope', 'Global', 'RssClient',
     function($rootScope, $scope, Global, RssClient) {
       $scope.global = Global;
-      $scope.message = 'Global';
+      $scope.moment = require('moment');
 
       $rootScope.$on('get-blog', function(event, obj){
         $scope.blogs = obj;
-        console.log(obj);
+      });
+
+      $rootScope.$on('get-qa', function(event, obj){
+        $scope.qa = obj;
+      });
+
+      $rootScope.$on('get-bbs', function(event, obj){
+        $scope.bbss= obj;
+      });
+
+      $rootScope.$on('get-map', function(event, obj){
+        $scope.map = obj;
       });
 
       RssClient.getBlog();
+      RssClient.getQA();
+      RssClient.getBBS();
+      RssClient.getMap();
     }
 ]);
--- packages/custom/bee-news-jp-client/public/services/rssClient.js
+++ packages/custom/bee-news-jp-client/public/services/rssClient.js
@@ -5,8 +5,23 @@ angular.module('mean.system')
       function($rootScope, $http) {
         return {
           getBlog: function(){
-            $http.jsonp("http://api.bee-news.net/blog.php?callback=JSON_CALLBACK").then(function(json) {
+            $http.jsonp("http://api.bee-news.net/blog.php?callback=JSON_CALLBACK&day=3").then(function(json) {
               $rootScope.$emit('get-blog', json.data);
+            });
+          },
+          getQA: function(){
+            $http.jsonp("http://api.bee-news.net/qa.php?callback=JSON_CALLBACK&day=3").then(function(json) {
+              $rootScope.$emit('get-qa', json.data);
+            });
+          },
+          getMap: function(){
+            $http.jsonp("http://api.bee-news.net/map.php?callback=JSON_CALLBACK&day=3").then(function(json) {
+              $rootScope.$emit('get-map', json.data);
+            });
+          },
+          getBBS: function(){
+            $http.jsonp("http://api.bee-news.net/bbs.php?callback=JSON_CALLBACK&day=3").then(function(json) {
+              $rootScope.$emit('get-bbs', json.data);
             });
           }
         }
